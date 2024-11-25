ab8a40f7cd514958c9cd3dd762c943ed548bd4e0
Maksym Didyk <didyk.maksym@gmail.com>
2024-11-25 18:48:02 +0200

    added page 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
 
-const Page404: FC = () => {
-  return <div>404</div>;
+import NotFound from '@/components/pages/NotFound/NotFound';
+import LocalizationProvider from '@/context/localization.context';
+import localizationService from '@/services/localization.service';
+
+
+const Page404: FC = async() => {
+  const localization = await localizationService.getLocalizations('uk', [
+    'COMMON',
+  ]);
+
+  return (
+    // <RootLayout localization={localization} themeColor={EColor.dark} hideFooter>
+    <LocalizationProvider localization={localization}>
+      <NotFound />
+    </LocalizationProvider>
+
+    // </RootLayout>
+
+  );
 };
 
 export default Page404;
