ed072c8bba14cfe46a1795fc26b4bd5d59127577
andiua <andr.babych@gmail.com>
2024-12-08 23:35:25 +0200

    fix message validation v.2

--- src/services/validation.service.ts
+++ src/services/validation.service.ts
@@ -1,6 +1,6 @@
 class Validation {
   readonly name = /^\p{Letter}(?:(?:[-\s`'"]?\p{Letter}){2,30}[\s]{0,1})$/u;
-  readonly message = /[\s\S]{3,}/g;
+  readonly message = /[\s\S]{3,}/;
   readonly phone = /^\+?[\d-()]{16}$/;
   readonly email = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/;
 }
