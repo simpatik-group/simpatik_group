a37e742d870b4c309b99dc557c4a20ba6324e581
andiua <andr.babych@gmail.com>
2024-12-09 20:15:36 +0200

    finish video

--- src/components/pages/Life/VideoLifePage/VideoLifePage.tsx
+++ src/components/pages/Life/VideoLifePage/VideoLifePage.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import ContainerUI from '@/components/ui/ContainerUI/ContainerUI';
+
 import { useMessages } from '@/hooks/useLocalization';
 
 import styles from './VideoLifePage.module.scss';
@@ -7,16 +9,18 @@ import styles from './VideoLifePage.module.scss';
 const VideoLifePage: FC = () => {
   const lifePage = useMessages('LIFE');
   return (
-    <section className='pageSection'>
-      <iframe
-        id='ytplayer'
-        width='720'
-        height='405'
-        src='https://www.youtube.com/embed/AK_rUXKZixI?color=white&showinfo=0&rel=0&modestbranding=0'
-        frameBorder='0'
-        allowFullScreen
-        className={styles.video}
-      />
+    <section>
+      <ContainerUI>
+        <iframe
+          id='ytplayer'
+          width='720'
+          height='405'
+          src={`${lifePage?.video_url}?color=white&showinfo=0&rel=0&modestbranding=0`}
+          frameBorder='0'
+          allowFullScreen
+          className={styles.video}
+        />
+      </ContainerUI>
     </section>
   );
 };
