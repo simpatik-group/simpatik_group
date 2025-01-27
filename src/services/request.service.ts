9996fb685c1035a7beaff2c39d0b767bbce96d68
Andrii Babych <andr.babych@gmail.com>
2025-01-27 12:07:40 +0200

    add bearer token; update vacancies page styling;

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -55,6 +55,7 @@ class RequestService {
       next: { revalidate: Number(process.env.REVALIDATING_TIME) },
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.NEXT_BEARER_TOKEN}`,
       },
     };
     if (options?.method) fetchOptions.method = options.method;
