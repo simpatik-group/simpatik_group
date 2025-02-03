b4de499ae571031e6167a12d5bc41818979ad401
Andrii Babych <andr.babych@gmail.com>
2025-02-03 17:40:51 +0200

    refactor metadata code; fix metadata error openGraph image

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,33 +9,6 @@ import { PageParams } from '@/interfaces/localozation';
 import { routing } from '@/i18n/i18n.config';
 import requestService from '@/services/request.service';
 
-// https://nextjs.org/docs/app/building-your-application/data-fetching/incremental-static-regeneration
-// генерація метаданих
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata,
-// ): Promise<Metadata> {
-//   // read route params
-//   // const id = (await params).id
-
-//   // fetch data
-//   const product = await fetch(
-//     process.env.NEXT_PUBLIC_DOMAIN + '/homepage?populate=*&locale=uk',
-//   ).then((res) => res.json());
-
-//   // optionally access and extend (rather than replace) parent metadata
-//   // const previousImages = (await parent).openGraph?.images || [];
-
-//   return {
-//     title: product.data.hero_title + ' | test',
-//   };
-// }
-// export function generateStaticParams() {
-//   return locales
-//     .filter((locale) => locale !== routing.defaultLocale)
-//     .map((locale) => ({ locale }));
-// }
-
 export default async function HomePage({ params }: { params: PageParams }) {
   const { lang } = (await params) as { lang: string };
   setRequestLocale(lang || routing.defaultLocale);
