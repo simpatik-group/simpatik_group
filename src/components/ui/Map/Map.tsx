691896757542bc0c28ac0892cf0e58bc922b073e
Andrii Babych <andr.babych@gmail.com>
2025-02-05 18:39:12 +0200

    add animated marker; move GeoJSON to separate component; prepare info panel if needed

--- src/components/ui/Map/Map.tsx
+++ src/components/ui/Map/Map.tsx
@@ -1,61 +1,31 @@
 'use client';
 
-import { FC, useEffect, useState } from 'react';
-import {
-  GeoJSON,
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  useMapEvents,
-} from 'react-leaflet';
+import { FC, useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import DriftMarker from 'react-leaflet-drift-marker';
 
 import { Icon, type LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import GeoJSONComponent from './GeoJSONComponent';
 import styles from './Map.module.scss';
 
-// import countriesBorders from './counties.json';
-
 const Map: FC = () => {
-  const positionUkraine: LatLngExpression = [49.8408, 31.08615];
-  const positionThai: LatLngExpression = [14.3842834, 100.6068409];
+  const initialCenter: LatLngExpression = [49.8408, 31.08615];
   const zoomDefault = 2;
-  const [countriesBorders, setCountriesBorders] = useState(null);
+  const [markerPosition, setMarkerPosition] =
+    useState<LatLngExpression>(initialCenter);
 
   const mapMarker = new Icon({
     iconUrl: '/favicon/favicon.svg',
-    iconSize: [27, 26],
+    iconSize: [39, 38],
   });
-  useEffect(() => {
-    fetch('/countries.json', { cache: 'force-cache' })
-      .then((res) => res.json())
-      .then((data) => {
-        setCountriesBorders(data);
-      });
-  }, []);
 
-  function LocationMarker() {
-    const [position, setPosition] = useState<LatLngExpression | null>(null);
-    const map = useMapEvents({
-      click() {
-        map.locate();
-      },
-      locationfound(e) {
-        setPosition(e.latlng);
-        // map.flyTo(e.latlng, map.getZoom());
-      },
-    });
-
-    return position === null ? null : (
-      <Popup position={position}>You are here</Popup>
-    );
-  }
   return (
     <div className={styles.wrapper}>
       <MapContainer
         style={{ height: '100%' }}
-        center={positionUkraine}
+        center={initialCenter}
         zoom={zoomDefault}
         attributionControl={false}
         scrollWheelZoom={false}
@@ -64,23 +34,16 @@ const Map: FC = () => {
         minZoom={zoomDefault}
         worldCopyJump={true}
       >
-        {/* <MapAddress positionDefault={positionDefault} zoomDefault={zoomDefault} /> */}
         <TileLayer
           url='https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'
           subdomains='abcd'
         />
-        {countriesBorders && (
-          <GeoJSON
-            data={countriesBorders}
-            style={{
-              fillColor: '#F1A21E',
-              color: '#F1A21E',
-              fillOpacity: 0.75,
-            }}
-          ></GeoJSON>
-        )}
-        <Marker position={positionUkraine} icon={mapMarker}></Marker>
-        {/* <LocationMarker /> */}
+        <DriftMarker
+          position={markerPosition}
+          icon={mapMarker}
+          duration={3000}
+        />
+        <GeoJSONComponent changeMarkerPosition={setMarkerPosition} />
       </MapContainer>
     </div>
   );
