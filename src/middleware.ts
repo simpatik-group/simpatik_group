776bc22c2988e381fb84326aecf5a33f045daa5c
Andrii Babych <33378360+babych-a@users.noreply.github.com>
2025-04-17 16:08:45 +0300

    Babych.a add service page Maintenance (#56)
    * fix charity load more;
    add work.ua link
    
    * add service page Maintenance

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,18 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { routing } from './i18n/i18n.config';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/maintenance')) {
+    return intlMiddleware(request);
+  }
+
+  // Redirect all other requests to /forbidden
+  return NextResponse.redirect(new URL('/maintenance', request.url));
+}
 
 export const config = {
   matcher: ['/', '/((?!api|_next|_vercel|.*\\..*).*)'],
