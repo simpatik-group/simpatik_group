f6a1d68d50bc244ff23d6bb108dda172a66254f1
Andrii Babych <andr.babych@gmail.com>
2025-04-17 16:07:20 +0300

    add service page Maintenance

---
+++ src/app/[lang]/maintenance/page.tsx
@@ -0,0 +1,9 @@
+import Maintenance from '@/components/pages/Maintenance/Maintenance';
+import { FC } from 'react';
+
+
+const MaintenancePage: FC = () => {
+	return <Maintenance />
+}
+
+export default MaintenancePage;
