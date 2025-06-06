d54bc7f927dda0ecb9c0f9f8d528a704e5d89598
Andrii Babych <33378360+babych-a@users.noreply.github.com>
2025-06-06 15:32:39 +0300

    turn site on (#60)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+
+import { routing } from './i18n/i18n.config';
 
 // for normal usage
-// import createMiddleware from 'next-intl/middleware';
-// import { routing } from './i18n/i18n.config';
-// export default createMiddleware(routing);
+export default createMiddleware(routing);
 
 // for maintenance mode
-export function middleware(request: NextRequest): NextResponse {
-  if (request.nextUrl.pathname.startsWith('/uk/maintenance')) {
-    return NextResponse.next();
-  }
-  return NextResponse.redirect(new URL('/uk/maintenance', request.url));
-}
+// export function middleware(request: NextRequest): NextResponse {
+//   if (request.nextUrl.pathname.startsWith('/uk/maintenance')) {
+//     return NextResponse.next();
+//   }
+//   return NextResponse.redirect(new URL('/uk/maintenance', request.url));
+// }
 
 export const config = {
   matcher: ['/', '/((?!api|_next|_vercel|.*\\..*).*)'],
